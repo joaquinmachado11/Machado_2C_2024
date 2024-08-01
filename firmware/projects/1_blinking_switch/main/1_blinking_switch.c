aa55e0216519f980fc366a2e09566d214d5ecb27
Joaquin Machado <joaquin.machado@ingenieria.uner.edu.ar>
2024-08-01 11:11:40 -0300

    Se sube resolución de ejercicio 2 de la guía 1. Se modificaron ademas los tiempos de parpadeo para cada LED.

--- firmware/projects/1_blinking_switch/main/1_blinking_switch.c
+++ firmware/projects/1_blinking_switch/main/1_blinking_switch.c
@@ -23,27 +23,60 @@
 #include "led.h"
 #include "switch.h"
 /*==================[macros and definitions]=================================*/
-#define CONFIG_BLINK_PERIOD 1000
+#define CONFIG_BLINK_PERIOD 500
 /*==================[internal data definition]===============================*/
-
+struct Tiempo 
+	{
+		uint8_t led1;
+		uint8_t led2;
+		uint8_t led3;
+	} tiempo;
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	uint8_t teclas;
-	LedsInit();
-	SwitchesInit();
+	uint8_t teclas;		// Variable entera de 8 bits que guarda el estado de las teclas.
+	LedsInit();			// Inicializo LEDs de la placa.
+	SwitchesInit();		// Inicializo switches de la placa.
+
+	tiempo.led1 = 100;	// a partir de struct defino los tiempos para cada LED
+	tiempo.led2 = 300;
+	tiempo.led3 = 1000;
+	
     while(1)    {
-    	teclas  = SwitchesRead();
-    	switch(teclas){
+    	teclas  = SwitchesRead();		// Leo el estado de los switches y lo guardo en teclas
+    	switch(teclas)					// switch con teclas
+		{
     		case SWITCH_1:
-    			LedToggle(LED_1);
+				printf("LED 1 titila\n");
+				while (teclas == SWITCH_1)	// mientras este activo SWITCH_1...
+				{
+    				LedToggle(LED_1);		// cambio el estado del LED
+					vTaskDelay(tiempo.led1 / portTICK_PERIOD_MS);
+					teclas  = SwitchesRead();	// Lee nuevamente el estado de los switches
+				}
+				LedOff(LED_1);		// Apago el LED la soltar el switch
     		break;
     		case SWITCH_2:
-    			LedToggle(LED_2);
+    			printf("LED 2 titila\n");
+				while (teclas == SWITCH_2)
+				{
+    				LedToggle(LED_2);
+					vTaskDelay(tiempo.led2 / portTICK_PERIOD_MS);
+					teclas  = SwitchesRead();
+				}
+				LedOff(LED_2);
     		break;
+			case SWITCH_1 | SWITCH_2:
+				printf("LED 3 titila\n");
+				while (teclas == (SWITCH_2 | SWITCH_1))
+				{
+    				LedToggle(LED_3);
+					vTaskDelay(tiempo.led3 / portTICK_PERIOD_MS);
+					teclas  = SwitchesRead();
+				}
+				LedOff(LED_3);
+				break;
     	}
-	    LedToggle(LED_3);
-		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
 	}
 }
