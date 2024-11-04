4679aa88c9220fd09cf7ed07116cf50c22ba4a64
Joaquin Machado <joaquin.machado@ingenieria.uner.edu.ar>
2024-11-04 18:00:42 -0300

    Obtencion de aceleracion terminado. Cambio de nombre de algunas variables y se borraron otras variables que no se les dio uso. Falta manejo de buzzer.

--- firmware/projects/examen/main/examen.c
+++ firmware/projects/examen/main/examen.c
@@ -41,19 +41,23 @@
 /*==================[macros and definitions]=================================*/
 #define TIEMPO_MUESTREO_DISTANCIA 500 // en ms
 #define TIEMPO_MUESTREO_ACELEROMETRO 10 //en ms
-#define GPIO_X GPIO_1
-#define GPIO_Y GPIO_2
-#define GPIO_Z GPIO_3
+#define GPIO_BUZZER GPIO_20	// lo configuro desde aca segun donde conecto el buzzer
+#define CH_X CH1	// lo configuro desde aca segun donde conecto los cables del modulo BT (si o si tienen que ir a los CH)
+#define CH_Y CH2
+#define CH_Z CH3
 /*==================[internal data definition]===============================*/
 TaskHandle_t distancia_task_handle = NULL;
 TaskHandle_t aceleracion_task_handle = NULL;
+uint8_t aceleracionDeCaida = 4; // [G]
 uint8_t tension_X;	// Tensiones que devuelve el acelerometro
 uint8_t tension_Y;
 uint8_t tension_Z;
-uint8_t tension_XYZ;
-uint8_t distancia;
-uint8_t sensibilidad = 0.3; // V/G
+uint8_t tension_XYZ;	// Suma de las tensiones del acelerometro
+uint8_t distance2car;	// distancia al auto
+uint8_t sensibilidad = 0.3; // [V/G]
 bool hayCaida = false;
+bool precaucion = false;
+bool peligro = false;
 /*==================[internal functions declaration]=========================*/
 void funcTimerDistancia(){
     vTaskNotifyGiveFromISR(distancia_task_handle, pdFALSE); /* Envía una notificación a la tarea asociada a pantalla */
@@ -63,12 +67,16 @@ void funcTimerAceleracion(){
     vTaskNotifyGiveFromISR(aceleracion_task_handle, pdFALSE);
 }
 
-static void enviarAdvertencia(){
-	if (distancia < 500 && distancia > 300)
+static void enviarAdvertencia(){	
+	if (precaucion){
 		UartSendString(UART_CONNECTOR, "Precaución, vehículo cerca.");
+		precaucion = false;
+	}
 
-	if (distancia < 300)
+	if (peligro){
 		UartSendString(UART_CONNECTOR, "Peligro, vehículo cerca.");
+		peligro = false;
+	}
 
 	if (hayCaida){
 		hayCaida = false;
@@ -76,23 +84,25 @@ static void enviarAdvertencia(){
 	}
 }
 
-static void medirDistancia (void *pvParameter){
+static void medirDistancia (void *pvParameter){		
     while(1){
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
-		distancia = HcSr04ReadDistanceInCentimeters();
+		distance2car = HcSr04ReadDistanceInCentimeters();
 
-		if (distancia > 500) {
+		if (distance2car > 500) {
             LedOn(LED_1);
             LedOff(LED_2);
             LedOff(LED_3);
             } else 
-		if (distancia < 500 && distancia > 300) {
-            LedOn(LED_1);
+		if (distance2car < 500 && distance2car > 300) {
+            precaucion = true;
+			LedOn(LED_1);
             LedOn(LED_2);
             LedOff(LED_3);
 			enviarAdvertencia();
             } else 
-		if (distancia < 300){
+		if (distance2car < 300){
+			peligro = true;
             LedOn(LED_1);
             LedOn(LED_2);
             LedOn(LED_3);
@@ -101,16 +111,21 @@ static void medirDistancia (void *pvParameter){
         }
 	}
 
-uint8_t tension2AcelerationConversion(){
+uint8_t tension2AcelerationConversion(){	
 	return tension_XYZ/sensibilidad;
 }
 
-static void obtenerAceleracion (void *pvParameter){		// a paritr
+static void obtenerAceleracion (void *pvParameter){		
     while(1){
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
 		
+		AnalogInputReadSingle(CH_X, &tension_X);
+		AnalogInputReadSingle(CH_Y, &tension_Y);
+		AnalogInputReadSingle(CH_Z, &tension_Z);
+
 		tension_XYZ = tension_X + tension_Y + tension_Z;
-		if (tension2AcelerationConversion() >= 4){
+
+		if (tension2AcelerationConversion() >= aceleracionDeCaida){
 			hayCaida = true;
 			enviarAdvertencia();
 		}
@@ -122,19 +137,26 @@ void app_main(void){
 	// Inicializacion de periféricos
     HcSr04Init(GPIO_3, GPIO_2);
     LedsInit();
-	BuzzerInit(GPIO_20);
-	GPIOInit(GPIO_X, GPIO_INPUT);
-	GPIOInit(GPIO_Y, GPIO_INPUT);
-	GPIOInit(GPIO_Z, GPIO_INPUT);
+	BuzzerInit(GPIO_BUZZER);
 
-	// Configuracion de ADC
-	analog_input_config_t analogIn = {
-		.input = CH1,
+	// Configuracion de ADCs
+	analog_input_config_t analogInX = {
+		.input = CH_X,
+		.mode = ADC_SINGLE
+	};
+	analog_input_config_t analogInY = {
+		.input = CH_Y,
+		.mode = ADC_SINGLE
+	};
+	analog_input_config_t analogInZ = {
+		.input = CH_Z,
 		.mode = ADC_SINGLE
 	};
 
-	// Inicializacion ADC
-	AnalogInputInit(&analogIn);
+	// Inicializacion ADCs
+	AnalogInputInit(&analogInX);
+	AnalogInputInit(&analogInY);
+	AnalogInputInit(&analogInZ);
 
     // Inicializacion de UART
     serial_config_t uart = {
@@ -164,7 +186,7 @@ void app_main(void){
 
     // Creacion de tareas
     xTaskCreate(&medirDistancia, "Medicion de distancia", 2048, NULL, 5, &distancia_task_handle);
-    xTaskCreate(&obtenerAceleracion, "Obtencio de aceleracion", 2048, NULL, 5, &aceleracion_task_handle);
+    xTaskCreate(&obtenerAceleracion, "Obtencion de aceleracion", 2048, NULL, 5, &aceleracion_task_handle);
 
 	// Inicio de los timers
     TimerStart(timer_acelerometro.timer);
